7915dedf8108fe550538becd10a41ee02ad1abe8
Fahad Kiani <fahadkiani@Fahads-Air.home>
2019-02-12 11:22:32 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-var x = "Joy Is Life";
+$(document).ready(function() {
+
+	$('h1').hover(function() {
+		$(this).css('background-color', '#FF0000');
+	})
+
+
+
+/* Scroll on Buttons */ 
+
+  $('.js--scroll-to-music').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-music').offset().top}, 1000); 
+    });
+
+
+   /* Animations on scroll */
+     $('.js--wp-1').hover(function(direction) {
+        $('.js--wp-1').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+       $('.js--wp-2').hover(function(direction) {
+        $('.js--wp-2').addClass('animated lightSpeedIn');
+    }, {
+        offset: '50%'
+    });
+
+});
+ 
